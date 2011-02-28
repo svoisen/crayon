0ff6b99a9e0e97eaa6e6f0f7f12a87737f930bb2
Sean Voisen <sean@voisen.org>
2011-02-28 15:07:46 -0800

    Add JSGenerator spec

---
+++ spec/js_generator_spec.rb
@@ -0,0 +1,51 @@
+# Copyright (c) 2010-2011 Sean Voisen.
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+$: << File.expand_path(File.dirname(__FILE__) + "/../lib")
+
+require 'rubygems'
+require 'treetop'
+require 'parser/crayon'
+require 'generator/js_generator'
+require 'snippets'
+
+module Crayon
+
+  describe Generator::JSGenerator do
+    def generate(code)
+      @generator.reset
+      @parser.parse(code).statements.first.codegen(@generator, true)
+    end
+
+    before(:all) do
+      @parser = Parser::CrayonParser.new
+      @generator = Generator::JSGenerator.new("js_generator_spec")
+    end
+
+    it "should generate variable assignments" do
+      generate(VAR_ASSIGN)
+      @generator.class_vars.first[:initializer].should == "this.x = 10;"
+      @generator.class_vars.first[:declaration].should == "p.x = null;"
+    end
+
+  end
+
+end
+
